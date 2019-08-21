782ae7f3582c95d7628cf5dcc48bcbc64a9d0cce
kellyfire <kellyfire611@gmail.com>
2019-08-21 18:26:54 +0700

    Bổ sung AJAX giao diện

--- quanlybanhang/resources/views/backend/pages/dashboard.blade.php
+++ quanlybanhang/resources/views/backend/pages/dashboard.blade.php
@@ -18,15 +18,38 @@ Màn hình Quản trị - Báo cáo nhanh tình hình Hệ thống
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tổng số Sản phẩm đang có trong Hệ thống</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">9999</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800" id="productCountText">
+                            Không biết
+                        </div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-calendar fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" id="btnRefreshProductCount">Refresh dữ liệu</button>
+            </div>
         </div>
     </div>
 
 </div>
+@endsection
+
+@section('custom-scripts')
+<script>
+$(document).ready(function() {
+    $('#btnRefreshProductCount').click(function(e) {
+        // Nhờ AJAX gởi request đến url /admin/api/getProductCount
+        $.ajax('{{ route('backend.api.getProductCount') }}', {
+            success: function (data) {
+                $('#productCountText').html(data.data[0].SoLuong + ' sản phẩm');
+            },
+            error: function () {
+                $('#productCountText').html('Không xử lý được!');
+            }
+        }); 
+    });
+});
+</script>
 @endsection
